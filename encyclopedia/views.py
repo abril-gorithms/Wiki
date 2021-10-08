1138a73fcadf0aa7526547545e08bfa7fb17668b
April Dariz Pintal <aprilpintal@gmail.com>
2021-10-08 22:24:23 +0800

    Committed all necessary subdirectories and files of Wiki Project

---
+++ encyclopedia/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from markdown2 import Markdown
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def entry_page(request, title):
+    pass
+
